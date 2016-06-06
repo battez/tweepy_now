168582ddd2486c6f2cf0fe2ec186459a0104b343
battez <lukebarker@gmail.com>
2016-06-06 16:17:00 +0100

    run comipled_france CSV query and store tweets to MLAB db

--- search.py
+++ search.py
@@ -48,14 +48,46 @@ post = {"author": "Luke","text": "My first blog post!"}
 client_alt = MongoClient(config.MLAB_URI)
 db_alt = client_alt.demo 
 
+
 def process_or_store(tweet):
     '''do something with tweets we get from Twitter API'''
     collection = db_alt['tweets']
-    collection.insert(tweet)
+    try:
+        collection.insert(tweet)
+    except:
+        logging.error('could not insert to db')
 
+def csv_reader(file_obj):
+    """
+    Read a csv file
+    """
+    import csv
+    reader = csv.reader(file_obj)
+    lines = []
+    for row in reader:
+        lines.append(row[0].strip())
+    return lines
+csv_file = 'compiled_france.csv'
+with open(csv_file, "r") as f_obj:
+    queries = csv_reader(f_obj)
 
-print('processing...')
+logging.info('Queries include:'+ ','.join(queries))
+since = " since:2016-05-30 "
+geos = {'midway_paris_sens':'48.5377029,2.4897794,30mi'}
+geo = geos['midway_paris_sens']
+ # geocode – Returns tweets by users located within a given radius of
+# the given latitude/longitude. The location is preferentially taking from 
+#the Geotagging API, but will fall back to their Twitter profile. The 
+# parameter value is specified by “latitide,longitude,radius”
+# might need to do until Saturday date so get max sweep of Wed Thu Fri
+qualify = since
+max_tweets = 2000
+for query in queries:
+    sleep(2) # in case we overstep our mongo lab free access
+    print('processing...', query + ' geocode:' + geo)
+    
+    for status in tweepy.Cursor(api.search, \
+        q=query + qualify, geocode=geo).items(max_tweets):
+        # Process a single status
+        process_or_store(status._json) # convenience: json from Status obj
 
-for status in tweepy.Cursor(api.user_timeline, id="vnf_officiel").items(100):
-    # Process a single status
-    process_or_store(status._json) # convenience to get json from Status obj
